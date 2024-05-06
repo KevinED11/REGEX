1b63d2f2638aad3f39471490a6c76d676ced26da
KevinED11 <mendez_kael@hotmail.com>
2024-05-05 21:47:04 -0700

    2

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import re
+
+
+def find_all_numbers(pattern: str, text: str) -> list[str]:
+    return re.findall(pattern, text)
+
+
 def main() -> None:
-    pass
+    result = find_all_numbers(r"\d", "The password is 12345")
+    print(result)
 
 
 if __name__ == "__main__":
